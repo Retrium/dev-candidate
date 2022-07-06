105aad48f9436a358c98085af285fd340168b8e5
Joshua McLucas <josh@retrium.com>
2022-07-06 16:28:48 -0400

    Set up vanilla tests

--- code-challenge/src/game.test.js
+++ code-challenge/src/game.test.js
@@ -1,1 +1,20 @@
-console.error('no tests!'); //JTM add test here
+function exampleTest(shouldFail) {
+  console.log('This is an example test');
+
+  if (shouldFail) {
+    throw new Error('Test failed');
+  }
+
+  console.log('Test passed');
+  return true;
+}
+
+function main() {
+  try {
+    exampleTest(true);
+  } catch(err) {
+    console.error(err.message);
+  }
+}
+
+main();
